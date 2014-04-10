588392bb23b17ccb7daeb081c2c73be6e2ba8347
Patrick Scanlan <patrick0392@gmail.com>
2014-04-10 04:33:43 -0400

    - Parallax Scrolling is set, the Scroll count variable is how many times it scrolls before it stops, and can be set to anything. - Menu buttons are hooked up and work as they should. - Added an "Infinite Scroll" which is just the trees of background1 so that the house doesn't repeat over and over.

--- Catastrophe/Assets/Scripts/MenuScript.cs
+++ Catastrophe/Assets/Scripts/MenuScript.cs
@@ -1,49 +1,65 @@
-﻿using UnityEngine;
+﻿
+
+using UnityEngine;
 
 /// <summary>
 /// Title screen script
 /// </summary>
 public class MenuScript : MonoBehaviour
 {
+	private GUISkin skin;
+	private GUISkin skin2;
+	
+	void Start()
+	{
+		// Load a skin for the buttons
+		skin = Resources.Load("StartBox") as GUISkin;
+		skin2 = Resources.Load ("OptionsBox") as GUISkin;
+	}
+	
 	void OnGUI()
 	{
-		const int buttonWidth = 84;
+		const int buttonWidth = 128;
 		const int buttonHeight = 60;
 		
+		// Set the skin to use
+		GUI.skin = skin;
+		
 		// Draw a button to start the game
-		if (
-			GUI.Button(
-
+		if (GUI.Button(
+			// Center in X, 2/3 of the height in Y
 			new Rect(
-			Screen.width / 8 ,
+			(Screen.width / 8) - (buttonHeight / 2),
 			(7 * Screen.height / 8) - (buttonHeight / 2),
 			buttonWidth,
 			buttonHeight
 			),
-			"Start!"
-			)
-			)
+			""
+			))
 		{
 			// On Click, load the first level.
-			Application.LoadLevel("catastrophe1");
+			Application.LoadLevel("catastrophe1"); 
 		}
 
-		if (
-			GUI.Button(
+		GUI.skin = skin2;
 
+		// Draw a button to start the game
+		if (GUI.Button(
+			// Center in X, 2/3 of the height in Y
 			new Rect(
-			(3 * Screen.width / 8) - (buttonWidth / 2),
+			(2 * Screen.width / 8) + (buttonHeight/2),
 			(7 * Screen.height / 8) - (buttonHeight / 2),
 			buttonWidth,
 			buttonHeight
 			),
-			"Options"
-			)
-			)
+			""
+			))
 		{
-			// On Click, load the options menu.
-			// Note: I haven't actually made the options menu yet, so it just loads the game.
-			Application.LoadLevel("catastrophe1");
+			// On Click, load the first level.
+			Application.LoadLevel("catastrophe1"); 
 		}
+
+
 	}
+	
 }
--- Catastrophe/Assets/Scripts/ScrollScript.cs
+++ Catastrophe/Assets/Scripts/ScrollScript.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class ScrollScript : MonoBehaviour {
 	/// <summary>
@@ -16,6 +17,31 @@ public class ScrollScript : MonoBehaviour {
 	/// Movement should be applied to camera
 	/// </summary>
 	public bool isLinkedToCamera = false;
+
+	//Background is infinite?
+	public bool isLooping = false;
+
+	//How many times to scroll?
+	public int scrollCount = 3;
+	private int scrolls = 0;
+
+	//List the children for the background
+	private List<Transform> backgroundPart;
+
+	//Get the children
+	void Start()
+	{
+				if (isLooping) {
+						backgroundPart = new List<Transform> ();
+						for (int i = 1; i < transform.childCount; i++) {
+								Transform child = transform.GetChild (i);
+								if (child.renderer != null) {
+										backgroundPart.Add (child);
+								}
+						}
+						backgroundPart = backgroundPart.OrderBy (t => t.position.x).ToList ();
+				}
+		}
 	
 	void Update()
 	{
@@ -33,5 +59,49 @@ public class ScrollScript : MonoBehaviour {
 		{
 			Camera.main.transform.Translate(movement);
 		}
-	}
+
+		if (isLooping)
+		{
+			Transform firstChild = backgroundPart.FirstOrDefault ();
+
+			if (firstChild != null)
+			{
+				// Check if the child is already (partly) before the camera.
+				// We test the position first because the IsVisibleFrom
+				// method is a bit heavier to execute.
+				if (firstChild.position.x < Camera.main.transform.position.x)
+				{
+					// If the child is already on the left of the camera,
+					// we test if it's completely outside and needs to be
+					// recycled.
+					if (firstChild.renderer.IsVisibleFrom(Camera.main) == false)
+					{
+						// Get the last child position.
+						Transform lastChild = backgroundPart.LastOrDefault();
+						Vector3 lastPosition = lastChild.transform.position;
+						Vector3 lastSize = (lastChild.renderer.bounds.max - lastChild.renderer.bounds.min);
+						
+						// Set the position of the recyled one to be AFTER
+						// the last child.
+						// Note: Only work for horizontal scrolling currently.
+						firstChild.position = new Vector3(lastPosition.x + lastSize.x, firstChild.position.y, firstChild.position.z);
+						
+						// Set the recycled child to the last position
+						// of the backgroundPart list.
+						backgroundPart.Remove(firstChild);
+						backgroundPart.Add(firstChild);
+						scrolls++;
+
+
+					}
+				}
+			}
+
+		}
+		if (scrolls > scrollCount)
+		{
+			isLooping = false;
+			speed = new Vector2(0,0);
+		}
+}
 }
